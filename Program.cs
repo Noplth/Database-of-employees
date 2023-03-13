d6bc19ed543837bf770aa087c29d0ffa26b16c6c
Noplth <andriy.golinovskiy@kpk-lp.com.ua>
2023-03-13 23:14:17 +0200

    feature/RS-1-Creating-basis

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace GlobalProperties
+{
+    public class MainMenu
+    {
+        public int NumChange;
+        public void Message()
+        {
+            Console.Write("Виберіть дію яку хочете виконати:\n1: Вибрати нового працівника\n" +
+                "2: Переглянути дані працівника\n" +
+                "3: Редагувати існуючуго працівника\n" +
+                "4: Видалити існуючого працівника \n" +
+                "5: Згенерувати звіт ");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MainMenu LetsGo = new MainMenu();
+            LetsGo.Message(); 
+        }
+    }
